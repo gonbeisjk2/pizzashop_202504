a69b55bff6af1cdb745f4da4909a9ea70a9cad43
taro shinjyuku <tarosjk@gmail.com>
2025-08-12 14:42:26 +0900

    追加画面のエラーチェック、リダイレクトの完成

--- add.php
+++ add.php
@@ -8,14 +8,16 @@ $errors = [
 
 // 入力値再反映用変数
 $pizzaname = '';
+$chefname = '';
+$toppings = '';
+
+// $error_flag = false;
 
 // 送信チェック
 if (isset($_POST['submit'])) {
-
-
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
+  // echo '<pre>';
+  // var_dump($_POST);
+  // echo '</pre>';
 
   // echo $_POST['pizza-name'];
   // echo '<br>';
@@ -24,16 +26,19 @@ if (isset($_POST['submit'])) {
   // 検証
   if (empty($_POST['pizza-name'])) {
     $errors['pizza-name'] = 'ピザの名前が入力されていません';
+    // $error_flag = true;
   } else {
     $pizzaname = $_POST['pizza-name'];
     if (!preg_match('/^([^\x01-\x7E]|[\da-zA-Z ])+$/', $_POST['pizza-name'])) {
       $errors['pizza-name'] = '日本語、英数字のみ有効です。記号等は使用できません';
+      // $error_flag = true;
     }
   }
 
   if (empty($_POST['chef-name'])) {
     $errors['chef-name'] = 'シェフの名前が入力されていません';
   } else {
+    $chefname = $_POST['chef-name'];
     if (!preg_match('/^([^\x01-\x7E]|[\da-zA-Z ])+$/', $_POST['chef-name'])) {
       $errors['chef-name'] = '日本語、英数字のみ有効です。記号等は使用できません';
     }
@@ -42,9 +47,24 @@ if (isset($_POST['submit'])) {
   if (empty($_POST['toppings'])) {
     $errors['toppings'] = 'トッピングが入力されていません';
   } else {
+    $toppings = $_POST['toppings'];
     if (!preg_match('/^([^\x01-\x7E]|[\da-zA-Z ])+$/', $_POST['toppings'])) {
       $errors['toppings'] = '日本語、英数字のみ有効です。記号等は使用できません';
     }
+  }
+
+  // エラーがあったかどうかをチェック
+  // if ($error_flag) {
+  //   echo '何らかのエラーがありました';
+  // }
+
+  // array_filter エラーメッセージを含む配列（エラーがあった場合）、もしくは空の配列(エラーがなかった場合)
+  // if文に要素がある配列を入れるとtrue, 空の配列を入れるとfalse
+  // エラーがなかった場合の処理
+  if (!array_filter($errors)) {
+    // TOPページへリダイレクト
+    header("location: index.php");
+    exit; //処理をここでストップする
   }
 }
 ?>
@@ -59,17 +79,17 @@ if (isset($_POST['submit'])) {
       <form action="add.php" method="post">
         <div class="mb-3">
           <label for="pizza-name" class="form-label">ピザの名前</label>
-          <input type="text" class="form-control" id="pizza-name" name="pizza-name" value="<?= $pizzaname; ?>">
+          <input type="text" class="form-control" id="pizza-name" name="pizza-name" value="<?= htmlspecialchars($pizzaname); ?>">
           <small class="form-text text-danger"><?= $errors['pizza-name']; ?></small>
         </div>
         <div class="mb-3">
           <label for="chef-name" class="form-label">シェフの名前</label>
-          <input type="text" class="form-control" id="chef-name" name="chef-name">
+          <input type="text" class="form-control" id="chef-name" name="chef-name" value="<?= htmlspecialchars($chefname); ?>">
           <small class="form-text text-danger"><?= $errors['chef-name']; ?></small>
         </div>
         <div class="mb-3">
           <label for="toppings" class="form-label">トッピング</label>
-          <input type="text" class="form-control" id="toppings" name="toppings">
+          <input type="text" class="form-control" id="toppings" name="toppings" value="<?= htmlspecialchars($toppings); ?>">
           <small class="form-text text-danger"><?= $errors['toppings']; ?></small>
         </div>
         <div class="text-center">
