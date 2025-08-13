0d5e3874f875c835abf4d3540ffb74788c48da1b
taro shinjyuku <tarosjk@gmail.com>
2025-08-13 14:40:52 +0900

    削除ボタンの追加

--- detail.php
+++ detail.php
@@ -24,25 +24,37 @@ if ($result) {
 <div class="container">
   <h1 class="h3 text-center">Pizza Detail</h1>
 
-  <div class="row justify-content-center">
-    <div class="col-lg-8">
-      <div class="card">
-        <img src="./img/pizza.jpg" alt="" class="card-img-top">
-        <div class="card-body">
-          <h2 class="h4"><?= htmlspecialchars($pizza['pizza_name']); ?></h2>
-          <p class="card-text">
-            <?= htmlspecialchars($pizza['toppings']); ?>
-          </p>
-          <p class="card-text text-secondary">
-            <?= htmlspecialchars($pizza['chef_name']); ?>
-          </p>
-          <p class="card-text text-end fs-6">
-            <?= htmlspecialchars($pizza['created_at']); ?>
-          </p>
+  <?php if (isset($pizza) && !empty($pizza)): ?>
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <div class="card">
+          <img src="./img/pizza.jpg" alt="" class="card-img-top">
+          <div class="card-body">
+            <h2 class="h4"><?= htmlspecialchars($pizza['pizza_name']); ?></h2>
+            <p class="card-text">
+              <?= htmlspecialchars($pizza['toppings']); ?>
+            </p>
+            <p class="card-text text-secondary">
+              <?= htmlspecialchars($pizza['chef_name']); ?>
+            </p>
+            <p class="card-text text-end fs-6">
+              <?= htmlspecialchars($pizza['created_at']); ?>
+            </p>
+          </div>
+          <div class="card-footer text-end">
+            <button class="btn btn-danger">削除</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  <?php else: ?>
+    <p class="alert alert-danger">
+      ピザの情報が存在しません
+    </p>
+    <div class="text-center">
+      <a href="index.php" class="btn btn-primary">TOPページへ戻る</a>
+    </div>
+  <?php endif; ?>
 </div>
 
 <?php include './template/footer.php' ?>
